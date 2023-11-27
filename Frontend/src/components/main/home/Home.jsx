4a1cb444ef98ed065fbd0f1df7937f557015f527
Yefferson Olivos <76671366+Yeronn@users.noreply.github.com>
2023-11-26 19:32:22 -0500

    Avances en pestaña Home

--- Frontend/src/components/main/home/Home.jsx
+++ Frontend/src/components/main/home/Home.jsx
@@ -1,7 +1,59 @@
 import './home.css'
 
 export default function Home () {
+  const price = 234234
   return (
-    <h1>Home</h1>
+    <section className='home'>
+      <div className='products-container'>
+        <article className='product-card'>
+          <div className='product-card--image-container'>Imagen</div>
+          <div className='product-card--price'>{price.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</div>
+          <span className='product-card--name'>Silla Escritorio Esgonomica Oficina de color negro ffff ffffffffffffffffffffffffffffffffff</span>
+          <span className='product-card--seller'>Vendido por</span>
+        </article>
+
+        <article className='product-card'>
+          <div className='product-card--image-container'>Imagen</div>
+          <div className='product-card--price'>{price.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</div>
+          <span className='product-card--name'>Silla Escritorio Esgonomica Oficina de color negro</span>
+          <span className='product-card--seller'>Vendido por</span>
+        </article>
+
+        <article className='product-card'>
+          <div className='product-card--image-container'>Imagen</div>
+          <div className='product-card--price'>{price.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</div>
+          <span className='product-card--name'>Silla Escritorio Esgonomica Oficina de color negro</span>
+          <span className='product-card--seller'>Vendido por</span>
+        </article>
+
+        <article className='product-card'>
+          <div className='product-card--image-container'>Imagen</div>
+          <div className='product-card--price'>{price.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</div>
+          <span className='product-card--name'>Silla Escritorio Esgonomica Oficina de color negro</span>
+          <span className='product-card--seller'>Vendido por</span>
+        </article>
+
+        <article className='product-card'>
+          <div className='product-card--image-container'>Imagen</div>
+          <div className='product-card--price'>{price.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</div>
+          <span className='product-card--name'>Silla Escritorio Esgonomica Oficina de color negro</span>
+          <span className='product-card--seller'>Vendido por</span>
+        </article>
+
+        <article className='product-card'>
+          <div className='product-card--image-container'>Imagen</div>
+          <div className='product-card--price'>{price.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</div>
+          <span className='product-card--name'>Silla Escritorio Esgonomica Oficina de color negro</span>
+          <span className='product-card--seller'>Vendido por</span>
+        </article>
+
+        <article className='product-card'>
+          <div className='product-card--image-container'>Imagen</div>
+          <div className='product-card--price'>{price.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</div>
+          <span className='product-card--name'>Silla Escritorio Esgonomica Oficina de color negro</span>
+          <span className='product-card--seller'>Vendido por</span>
+        </article>
+      </div>
+    </section>
   )
 }
